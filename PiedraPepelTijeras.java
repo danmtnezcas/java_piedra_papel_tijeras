fcb8c0ae916de2141b0a62b33a3264c9d88ac2f5
danmtnezcas <danmtnezcas@outlook.com>
2023-03-31 10:42:47 -0600

    Mostrando instrucciones al usuario

--- PiedraPepelTijeras.java
+++ PiedraPepelTijeras.java
@@ -6,7 +6,14 @@ public class PiedraPepelTijeras {
     private static final String TIJERAS = "T";
     private static final String SALIR = "S";
 
+    // Mensajes al usuario
+    private static final String BIENVENIDA = "Bienvenido al juego Piedra-Papel-Tijeras";
+    private static final String PEDIR_JUGADA = "¿Cuál es tu jugada? " + PIEDRA + " (piedra), " + PAPEL + " (papel), " + TIJERAS + " (tijeras) o " + SALIR + " (salir)";
+
     public static void main(String[] args) {
+        // Instrucciones
+        System.out.println(BIENVENIDA);
+        System.out.println(PEDIR_JUGADA);
         
     }
 }
