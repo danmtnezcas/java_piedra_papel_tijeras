54db6705e91ae91d5e9c05675a590ba3f1c5a4c7
danmtnezcas <danmtnezcas@outlook.com>
2023-03-31 10:36:00 -0600

    Agregando las constantes para las opciones disponibles

--- PiedraPepelTijeras.java
+++ PiedraPepelTijeras.java
@@ -1,5 +1,11 @@
 
 public class PiedraPepelTijeras {
+    // Constantes para las opciones disponibles
+    private static final String PIEDRA = "P";
+    private static final String PAPEL = "L";
+    private static final String TIJERAS = "T";
+    private static final String SALIR = "S";
+
     public static void main(String[] args) {
         
     }
